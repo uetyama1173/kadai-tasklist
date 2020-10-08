e13c4db424d69c3f288d33ee0bdcc4ad170f0e5c
shou.ueda <show_0310k@yahoo.co.jp>
2020-10-08 03:55:08 +0000

    resubmit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:show]
   
   def index
      @tasks = current_user.tasks.order(id: :asc)
@@ -60,18 +60,17 @@ class TasksController < ApplicationController
 private
 
 
-
-
-def set_task
+def set_task 
   @task = Task.find(params[:id])
 end
+
 
 def task_params
  params.require(:task).permit(:content, :status)
 end
 
 def correct_user
-    @task = current_user.tasks.find_by(id: params[:id])
+  @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to login_url
     end
